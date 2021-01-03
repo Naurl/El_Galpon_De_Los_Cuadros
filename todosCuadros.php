f6fb2b5af2030ee670b7822920b0309ae1a9e2e8
Nahuel Venencio <nahuelvenecio@gmail.com>
2021-01-02 21:38:06 -0300

    Agregado de version mas actualizada del codigo de JQuery, quedaria pendiente investigar si hay alguna opcion de utilizar multiples fuentes para el codigo fuente de JQuery

--- todosCuadros.php
+++ todosCuadros.php
@@ -33,7 +33,11 @@
         ?>
     </div>
   </section>
-  <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+  <!-- <script src="https://code.jquery.com/jquery-1.9.1.min.js" integrity="sha256-wS9gmOZBqsqWxgIVgA8Y9WcQOa7PgSIX+rPA0VL2rbQ=" crossorigin="anonymous"></script> -->
+  <script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
   <script src="scripts/todosCuadrosEvents.js"></script>
 </body>
 </html>
